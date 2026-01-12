8c447aa0058df3eed657dccfc8fea286f665b9c9
skytbac <skytbac@outlook.com>
2026-01-12 11:48:18 +0800

    Add Copyright

--- Chipyard/Boom/tile.scala
+++ Chipyard/Boom/tile.scala
@@ -3,6 +3,16 @@
 // All Rights Reserved. See LICENSE and LICENSE.SiFive for license details.
 //------------------------------------------------------------------------------
 
+// Copyright (c) 2025 ICRG@Shanghai Jiao Tong University
+// PolymorPIC is licensed under Mulan PSL v2.
+// You can use this software according to the terms and conditions of the Mulan PSL v2.
+// You may obtain a copy of Mulan PSL v2 at:
+//          http://license.coscl.org.cn/MulanPSL2
+// THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+// EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+// MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+// See the Mulan PSL v2 for more details.
+
 package boom.common
 
 import chisel3._
